4061fa6643cdeb180510575ba04c28cb3e41ffa7
userArbus <109461770+userArbus@users.noreply.github.com>
2022-07-18 14:59:06 +0530

    Create def.py

---
+++ def.py
@@ -0,0 +1,12 @@
+def new(x):
+       print("fuk u "+ x)
+       print("ur mum "+ x)
+       print ("suck my "+x)
+new("pythonbot") 
+
+
+
+
+pov alt of help()~
+import inspect, random
+print(inspect.getdoc(random))
