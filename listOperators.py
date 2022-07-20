9bc6a867b82d4e0d0ddd19efe09814399e10a8fd
userArbus <109461770+userArbus@users.noreply.github.com>
2022-07-20 23:47:49 +0530

    Create listOperators.py

---
+++ listOperators.py
@@ -0,0 +1,5 @@
+spam = spam + 1                        spam += 1            #list concatenation
+spam = spam - 1                        spam -= 1            #substraction
+spam = spam * 1                        spam *= 1            #list replication
+spam = spam / 1                        spam /= 1            #divide
+spam = spam % 1                        spam %= 1            #remainder finder
