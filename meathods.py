18c8eded86be910314a97c37230df51a7149759b
userArbus <109461770+userArbus@users.noreply.github.com>
2022-07-20 22:43:20 +0530

    Create meathods.py

---
+++ meathods.py
@@ -0,0 +1,14 @@
+ spam = ['hello', 'hi', 'howdy', 'heyas']
+ spam.index('hello')                                  #tell the order of item
+ spam = ['cat', 'dog', 'bat']           
+ spam.append('moose')                                 #adds item in back of list
+ spam = ['cat', 'dog', 'bat'] 
+ spam.insert(1, 'chicken')                            #adds item in given index
+ spam = ['cat', 'bat', 'rat', 'elephant']
+ spam.remove('bat')                                   #purges out a item
+ spam = [2, 5, 3.14, 1, -7]
+ spam.sort()                                          #sort according to ascii
+ spam.sort(reverse=True)                              #sord in reverse of ascii
+ spam = ['a', 'z', 'A', 'Z']
+ spam.sort(key=str.lower)                             #sort lower key first and then accordingly
+  
